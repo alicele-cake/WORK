efaf3136d43b84de0a0ef30fdf6b1d05288bafa5
alice <qwe821526@gmail.com>
2024-10-04 21:21:53 +0800

    Delete shop/shop/update_comment.php

--- shop/shop/update_comment.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    include("datatable.php");
-$id = $_POST["id"];
-// $name=$_POST["name"];
-    $comment = $_POST['comment'];
-$statement = updateComment($id, $comment);
-    header('Location:/Guestbook/index.php');
